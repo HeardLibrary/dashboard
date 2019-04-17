1305bd41db743e5630b0e23080f3b6ea515f13bf
Steve Baskauf <steve.baskauf@vanderbilt.edu>
2019-04-16 20:11:31 -0500

    add time zone support to weather app

--- lambdas/weather.py
+++ lambdas/weather.py
@@ -3,6 +3,7 @@ import boto3
 import json
 import os
 import datetime
+import pytz
 
 def lambda_handler(event, context):
     locationString = '16133_PC' # Pleasant View, TN 37146
@@ -10,11 +11,16 @@ def lambda_handler(event, context):
     url = 'http://dataservice.accuweather.com/currentconditions/v1/' + locationString
     r = requests.get(url, params={'apikey': apiKey, 'details': 'true'})
     data = r.json()
-    isoDate = data[0]['LocalObservationDateTime'][0:10]
+    # create datetime object with UTC timezone
+    utc_now = pytz.utc.localize(datetime.datetime.utcnow())
+    # convert the datetime object to US Central time zone
+    central_now = utc_now.astimezone(pytz.timezone("US/Central"))
+    now_iso = central_now.isoformat()
+    isoDate = now_iso[0:10]
     pptInches = data[0]['PrecipitationSummary']['Past24Hours']['Imperial']['Value'] # value is float, I think
     minTempF = data[0]['TemperatureSummary']['Past24HourRange']['Minimum']['Imperial']['Value'] # value is float, in deg F
     maxTempF = data[0]['TemperatureSummary']['Past24HourRange']['Maximum']['Imperial']['Value'] # value is float, in deg F
-    print(datetime.datetime.utcnow().isoformat())
+    print(now_iso)
     print(' Min temp in past 24 hours: ' + str(minTempF))
     print(' Max temp in past 24 hours: ' + str(maxTempF))
     print(' Inches precipitation in past 24 hours: ' + str(pptInches))
